172cf1bab19374cff1430ea8133d61e6fb3d8922
Julie Jaasma <31329@ma-web.nl>
2023-09-15 12:22:24 +0200

    Idk

--- Bo3/Infection/Assets/scripts/Damageplayer.cs
+++ Bo3/Infection/Assets/scripts/Damageplayer.cs
@@ -7,6 +7,13 @@ public class Damageplayer : MonoBehaviour
     [SerializeField] private int Hitdamage;
     [SerializeField] private HealthBar healthbar;
 
+
+    //dit script doet 2 dingen
+    //hij kijkt of er een collision is en hij geeft de speler damage
+    // daarom is dit niet SRP
+    // dit moet ook los van elkaar kunnen
+
+
     private void Start()
     {
         healthbar = GameObject.Find("HealthController").GetComponent<HealthBar>();
